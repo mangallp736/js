7317fac56024f7458fffe85febea87e92ee58c0f
mangallp736 <mangalp736@gmail.com>
2026-04-01 23:34:58 +0530

    Array interview question JS

---
+++ interview_array.js
@@ -0,0 +1,27 @@
+//// Group Array Items by Property 
+///Interview Question from Capgemini
+
+/**
+ * 
+ * const users = [
+ *  { name: "Mohit", city: "Delhi" },
+ * { name: "Amit", city: "Delhi" },
+ * {name: "Rohit", city: "Noida" },
+ * ];
+ */
+
+const users = [
+   { name: "Mohit", city: "Delhi" },
+  { name: "Amit", city: "Delhi" },
+  {name: "Rohit", city: "Noida" },
+  ];
+
+const grouped = users.reduce((acc, user) => {
+    (acc[user.city]=acc[user.city] || []).push(user);
+    return acc;
+}, {});
+
+console.log(grouped);
+
+////acc = accumulator
+
